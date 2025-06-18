d4277742f27f081d18aa2b47e4af792d47a2ffa8
Maharaja95v <136450869+Maharaja95v@users.noreply.github.com>
2025-06-18 22:16:42 +0530

    Update llm_helper.py

--- Post-Generator-main/project-genai-post-generator-main/llm_helper.py
+++ Post-Generator-main/project-genai-post-generator-main/llm_helper.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY","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"), model_name="llama-3.1-8b-instant")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-8b-instant")
 
 
 if __name__ == "__main__":
