56a1127016c26eb0a981489bc34b4422d1e1dec6
Maharaja95v <136450869+Maharaja95v@users.noreply.github.com>
2025-06-18 22:42:17 +0530

    Update few_shot.py

--- Post-Generator-main/project-genai-post-generator-main/few_shot.py
+++ Post-Generator-main/project-genai-post-generator-main/few_shot.py
@@ -8,9 +8,9 @@ class FewShotPosts:
         if file_path is None:
             base_path = os.path.dirname(__file__)  # path of few_shot.py
             file_path = os.path.join(base_path, "data", "processed_posts.json")
-        self.load_posts(file_path)
         self.df = None
         self.unique_tags = None
+        self.load_posts(file_path)
 
     def load_posts(self, file_path):
         with open(file_path, encoding="utf-8") as f:
